237431cfbc5e9a5a45fac2611981f2bb49b57a60
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-10-26 15:53:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist = new Object({
-  [Slowdive] : 'Alison',
-  [My Bloody Valentine] : 'Sometimes'
+  ['Slowdive'] : 'Alison',
+  ['My Bloody Valentine'] : 'Sometimes'
 });
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[Phil Ochs] = "Here's to the State of Mississippi"
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   return playlist
 }
 
